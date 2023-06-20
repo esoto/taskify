625155ca15bb08e46ea877b6b732cc1c70105a90
Esteban Soto <esoto074@gmail.com>
2023-06-19 21:10:09 -0600

    Simple turbo app to demonstrate how to use turbo with tailwindcss Edit and delete tasks happens with turbo frames

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :tasks
+  root "tasks#index"
+  
+  resources :tasks do
+    post "/toggle", to: "tasks#toggle", as: "toggle"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
